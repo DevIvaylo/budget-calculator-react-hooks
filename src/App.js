9dc83d2ff39c27ab7635ae7dc5954d10f9fe304c
Ivaylo | IG <ivayloig@apples-mbp.lan>
2020-04-17 16:38:56 +0100

    useState setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import ExpenseForm from "./components/ExpenseForm";
 import ExpenseList from "./components/ExpenseList";
 import Alert from "./components/Alert";
+import uuid from 'uuid/v4';
+
+const initialExpenses = [
+    {id: uuid(), charge: 'rent', amount: 1300},
+    {id: uuid(), charge: 'car insurance', amount: 1900},
+    {id: uuid(), charge: 'credit card bill', amount: 160}
+];
 
 function App() {
+    // array destructuring
+    const [expenses, setExpenses] = useState(initialExpenses);
+    console.log(expenses);
+    console.log(setExpenses);
+
     return (
         <>
             <Alert/>
