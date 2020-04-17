c229f545b0c234d6476b3af073e96d6abc1c26ff
Ivaylo | IG <ivayloig@apples-mbp.lan>
2020-04-17 18:11:04 +0100

    ExpenseForm skeleton

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import {MdSend} from "react-icons/all";
 
 const ExpenseForm = () => {
     return (
-        <>
-            <h2>expense form</h2>
-        </>
+        <form>
+            <div className='form-center'>
+                <div className="form-group">
+                    <label htmlFor="charge">charge</label>
+                    <input type="text" className="form-control" placeholder='e.g rent' id='charge' name='charge'/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="amount">amount</label>
+                    <input type="text" className="form-control" placeholder='e.g 100' id='amount' name='amount'/>
+                </div>
+            </div>
+            <button type='submit' className='btn'>submit <MdSend className='btn-icon'/></button>
+        </form>
     )
 };
 
