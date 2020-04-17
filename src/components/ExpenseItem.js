7f7cc0f2582ce7e9ad80a88394635b627665cc16
Ivaylo | IG <ivayloig@apples-mbp.lan>
2020-04-17 18:02:21 +0100

    implementing ExpenseItem component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import {MdEdit, MdDelete} from "react-icons/all";
 
-const ExpenseItem = () => {
+const ExpenseItem = ({expense}) => {
+    const {id, charge, amount} = expense;
     return (
-        <>
-           <li></li>
-        </>
+        <li className="item">
+            <div className="info">
+                <span className='charge'>{charge}</span>
+                <span className='amount'>{amount}</span>
+            </div>
+            <div>
+                <button className='edit-btn' aria-label='edit button'><MdEdit/></button>
+                <button className='clear-btn' aria-label='delete button'><MdDelete/></button>
+            </div>
+        </li>
     )
 };
 
